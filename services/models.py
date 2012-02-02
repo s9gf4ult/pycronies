e8a5aa6ae267b79c39f311f832aaa7f097aa67b6
Alex <s9gf4ult@gmail.com>
2012-02-02 18:25:11 +0600

    many things commited into model

--- services/models.py
+++ services/models.py
@@ -3,8 +3,29 @@
 from django.db import models
 from datetime import date, datetime
 import uuid
+import re
 
 # Create your models here.
+
+class SafeTextField(models.TextField):
+    def get_prep_value(self, value):
+        """Validate value and return prepared
+        Arguments:
+        - `value`:
+        """
+        if re.search('[<>]', value):
+            raise models.validators.ValidationError('Text field must not contain < or > symbols')
+        return super(SafeTextField, self).get_prep_value(value)
+
+class SafeCharField(models.SlugField):
+    def get_prep_value(self, value):
+        """Validate field and return prepared value
+        Arguments:
+        - `value`:
+        """
+        if re.search('[<>]', value):
+            raise models.validators.ValidationError('Text field must not contain < or > symbols')
+        return super(SafeCharField, self).get_prep_value(value)
 
 def hex4():
     """Return hexdigest of uuid4
@@ -27,20 +48,24 @@ class BaseModel(models.Model):
 class Project(BaseModel):
     """Проект
     """
-    name = models.CharField(max_length=100)
-    descr = models.TextField(null=True)
+    PROJECT_RULESET=((u'despot', u'Проект управляется инициатором'),
+                     (u'vote', u'Проект управляется голосованием'),
+                     (u'auto', u'Проект управляется автоматически'))
+    PROJECT_STATUS=((, ), )               # FIXME: написать какие статусы у проекта бывают
+    name = SafeCharField(max_length=100, default=None)
+    descr = SafeTextField(default=u'')
     sharing = models.BooleanField()
-    ruleset = models.CharField(max_length=20)
+    ruleset = models.CharField(max_length=40, default='despot', null=False, choices=PROJECT_RULESET)
     begin_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
-    status = models.CharField(max_length=20)
+    status = models.CharField(max_length=40, null=False, choices=PROJECT_STATUS)
 
 class Activity(BaseModel):
     """Мероприятие
     """
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    name = models.CharField(max_length = 100)
-    descr = models.TextField(null=True)
+    name = SafeCharField(max_length=100)
+    descr = SafeTextField()
     begin_date = models.DateTimeField(null=True)
     end_date = models.DateTimeField(null=True)
     accept = models.BooleanField()
@@ -51,99 +76,174 @@ class Activity(BaseModel):
 class Participant(BaseModel):
     """Участрник проекта
     """
-    user = models.CharField(max_length=40, null=True)   # FIXME: Это должна быть ссылка на чето, пока заглушка
     project = models.ForeignKey(Project)
-    name = models.CharField(max_length=100)
-    descr = models.TextField(null=True)
-    accept = models.BooleanField()
+    is_initiator = models.BooleanField(default=False)
+    user = models.CharField(max_length=40, null=True)   # FIXME: Это должна быть ссылка на пользюка, пока заглушка
+    psid = models.CharField(max_length=40)
+    name = SafeCharField(max_length=100, default=None, null=False)
+    descr = SafeTextField(default=u'')
+    accept = models.BooleanField()        # FIXME: Заменить на статус с возможными значениями как для ресурсов мероприятия ?
+    last_login = models.DateTimeField(null=True)
 
     class Meta:
         unique_together = (("project", "name"), )
 
-class Resource(BaseModel):
-    """Ресурс проекта
+class MeasureUnits(BaseModel):
+    """Еденицы измерения количества ресурса
     """
+    name = SafeCharField(max_length=50)
+    descr = SafeTextField()
+
+class Resource(BaseModel):
+    """Доступный ресурс проекта
+    """
+    USAGE_CHOISES = ((u'personal', u'Можно использовать как личный ресурс'),
+                     (u'common', u'Можно использовать только как общий'))
     project = models.ForeignKey(Project)
     product = models.CharField(max_length=100, null=True) # FIXME: Это должна быть ссылка на продукт !!!
-    name = models.CharField(max_length=100)
-    descr = models.TextField()
-    measure = models.CharField(max_length=40) # FIXME: Может это будет ссылка на элемент таблицы "еденицы измерения" ?
-    usage = models.CharField(max_length=40)
+    name = SafeCharField(max_length=100, null=False, default=None)
+    descr = SafeTextField(default=u'')
+    measure = models.ForeignKey(MeasureUnits)
+    usage = models.CharField(max_length=40, choices=USAGE_CHOISES)
     site = models.CharField(max_length=40)
-    
+
     class Meta:
         unique_together = (("project", "product"),
                            ("project", "name"))
 
-class ActPart(BaseModel):
+class ActivityParticipant(BaseModel):
     """Участник мероприятия
     """
-    project = models.ForeignKey(Project)
     activity = models.ForeignKey(Activity, on_delete = models.CASCADE)
     participant = models.ForeignKey(Participant, on_delete = models.CASCADE)
-    vote = models.CharField(max_length=40)
-    
-class ActRes(BaseModel):
-    """Ресурс мероприятия или личный участника мероприятия
-    """
-    project = models.ForeignKey(Project)
-    activity = models.ForeignKey(Activity, on_delete = models.CASCADE)
-    participant = models.ForeignKey(ActPart, null=True, on_delete = models.CASCADE)
-    resource = models.ForeignKey(Resource)
-    vote = models.CharField(max_length=40, null=True)   # NOTE:  можно null ?
-    required = models.BooleanField(default=False)
-    amount = models.DecimalField(max_digits=20, decimal_places = 2, null=False, default=None)
-    accept = models.BooleanField()
-
+    accept = models.BooleanField(default = False)
+    vote = models.CharField(max_length=40)   # FIXME: Заменить на статус с возможными значениями как в ресурсах мероприятия ?
     class Meta:
-        unique_together = (("project", "activity", "participant", "resource"), )
+        unique_together = (("activity", "participant"), )
 
-class Parameter(BaseModel):
-    """Описание параметра
+class ActivityResource(BaseModel):
+    """Ресурс мероприятия
     """
-    name = models.SlugField()
-    descr = models.TextField()
-    tp = models.CharField(max_length=40)
-    enum = models.BooleanField()
-    default_value = models.CharField(max_length=40)
-
-class Param(BaseModel):
-    """Параметр
-    """
-    project = models.ForeignKey(Project)
+    ACTIVITY_RESOURCE_STATUS=((u'accepted', u'Ресурс доступен в мероприятии'),
+                              (u'denied', u'Ресурс исключен из мероприятия'),
+                              (u'voted', u'Ресурс предложен для использования в мероприятии'))
     activity = models.ForeignKey(Activity)
     resource = models.ForeignKey(Resource)
-    parameter = models.ForeignKey(Parameter)
-    name = models.CharField(max_length=100)
-    descr = models.TextField(null=True)
-    level = models.CharField(max_length=40)
+    required = models.BooleanField(default=False)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, null=False, default=None) # NOTE: так как за количество ресурса в мероприятии тоже будут голосовать, а таблица голосов содержит ForeignKey на параметр ресурса, то может количество тоже сделать параметром ресурса ??
+    status = models.CharField(max_length=40, default=u'voted', choices=ACTIVITY_RESOURCE_STATUS)
+    class Meta:
+        unique_together = (("activity", "resource"), )
+
+class ParticipantResource(BaseModel):
+    """Личный ресурс участника мероприятия
+    """
+    actpart = models.ForeignKey(ActivityParticipant)
+    resource = models.ForeignKey(Resource)
+    amount = models.DecimalField(max_digits=20, decimal_places=2, null=False, default=None)
+    class Meta:
+        unique_together = (("actpart", "resource"), )
+
+class ActivityResourceVote(BaseModel):
+    """Предложение на включение ресурса в мероприятие
+    """
+    ACTIVITY_RESOURCE_VOTE = ((u'include', u'Включить ресурс в мероприятие'),
+                              (u'exclude', u'Исключить ресурс из мероприятия'),
+                              (u'add', u'Добавить или отнять количество ресурса'))
+    ACTIVITY_RESOURCE_VOTE_STATUS = ((u'voted', u'Предложено'),
+                                     (u'accepted', u'Принято'),
+                                     (u'denied', u'Отклонено'),
+                                     (u'imposed', u'Вынесено на голосование'))
+    resource = models.ForeignKey(ActivityResource)
+    participant = models.ForeignKey(Participant)
+    vote = models.CharField(max_length=40, null=False, default=None, choices = ACTIVITY_RESOURCE_VOTE)
+    status = models.CharField(max_length=40, null=False, default=u'voted', choices = ACTIVITY_RESOURCE_VOTE_STATUS)
+
+# class ActRes(BaseModel):
+#     """Ресурс мероприятия или личный участника мероприятия
+#     """
+#     project = models.ForeignKey(Project)
+#     activity = models.ForeignKey(Activity, on_delete = models.CASCADE)
+#     participant = models.ForeignKey(ActPart, null=True, on_delete = models.CASCADE)
+#     resource = models.ForeignKey(Resource)
+#     vote = models.CharField(max_length=40, null=True)   # NOTE:  можно null ?
+#     required = models.BooleanField(default=False)
+#     amount = models.DecimalField(max_digits=20, decimal_places = 2, null=False, default=None)
+#     accept = models.BooleanField()
+
+#     class Meta:
+#         unique_together = (("project", "activity", "participant", "resource"), )
+
+class DefaultParameter(BaseModel):
+    """Предлагаемый параметр
+    """
+    name = SafeCharField(max_length=100, default=None)
+    descr = SafeTextField(default=u'')
+    tp = models.CharField(max_length=40)
+    enum = models.BooleanField(default = False)
+    default_value = models.CharField(max_length=40, default=None)
+
+class DefaultParameterVl(BaseModel):
+    """Перечисляемое значение предлагаемого параметра
+    """
+    parameter = models.ForeignKey(DefaultParameter)
+    value = models.CharField(max_length=40, default=None, null=False)
+    caption = models.TextField()
+
+class BaseParameter(BaseModel):
+    """Базовый класс для параметров
+    """
+    default_parameter = models.ForeignKey(DefaultParameter, null=True)
+    name = SafeCharField(max_length=100, default=None, null=False)
+    descr = SafeTextField(default=u'')
     tp = models.CharField(max_length=40)
     enum = models.BooleanField()
+    class Meta:
+        abstract = True
 
-class ParamVl(BaseModel):
-    """Значение параметра
+class BaseParameterVl(BaseModel):
+    """Базовый класс перечисляемых значений параметра
     """
-    param = models.ForeignKey(Param)
     value = models.CharField(max_length=40)
     caption = models.TextField()
+    class Meta:
+        abstract = True
 
-class ParameterVL(BaseModel):
-    """Возможное значение описания параметра
+class BaseParameterVal(BaseModel):
+    """Базовый класс значения параметра
     """
-    parameter = models.ForeignKey(Parameter)
     value = models.CharField(max_length=40)
     caption = models.TextField()
-
-class ParamVal(BaseModel):
-    """Значения параметра
-    """
-    param = models.ForeignKey(Param)
-    participant = models.ForeignKey(Participant, null=True)
-    value = models.CharField(max_length=40)
-    caption = models.TextField()
-    datetime = models.DateTimeField()
     opened = models.BooleanField()
-    level = models.CharField(max_length=40)
+    class Meta:
+        abstract = True
+
+class ProjectParameter(BaseParameter):
+    project = models.ForeignKey(Project)
+class ProjectParameterVl(BaseParameterVl):
+    parameter = models.ForeignKey(ProjectParameter)
+class ProjectParameterVal(BaseParameterVal):
+    parameter = models.ForeignKey(ProjectParameter)
+
+class ActivityParameter(BaseParameter):
+    activity = models.ForeignKey(Activity)
+class ActivityParameterVl(BaseParameterVl):
+    parameter = models.ForeignKey(ActivityParameter)
+class ActivityParameterVal(BaseParameterVal):
+    parameter = models.ForeignKey(ActivityParameter)
+
+class ResourceParameter(BaseParameter):
+    resource = models.ForeignKey(Resource)
+class ResourceParameterVl(BaseParameterVl):
+    parameter = models.ForeignKey(ResourceParameter)
+class ResourceParameterVal(BaseParameterVal)
+
+class ParticipantParameter(BaseParameter):
+    participant = models.ForeignKey(Participant)
+class ParticipantParameterVl(BaseParameterVl):
+    parameter = models.ForeignKey(ParticipantParameter)
+class ParticipantParameterVal(BaseParameterVal):
+    parameter = models.ForeignKey(ParticipantParameter)
 
 class PartContakt(BaseModel):
     """Контакт участника проекта
