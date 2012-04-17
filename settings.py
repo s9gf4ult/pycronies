3926a4e7569e7cea8062a217afa7ffa5ba64475d
segfault <s9gf4ult@gmail.com>
2012-04-17 10:19:13 +0600

    statifeils

--- settings.py
+++ settings.py
@@ -61,8 +61,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-if not DEBUG:
-    STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
